8f0082726a560135500627d097fead7eeaedf447
Monjur Elahi Touheed <monjurelahitouheed@gmail.com>
2025-06-12 16:10:09 +0600

    first commit

---
+++ src/components/Root/Root.jsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router";
+import Footer from "../Footer/Footer";
+
+const Root = () => {
+    return (
+        <div>
+            <Outlet></Outlet>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Root;
