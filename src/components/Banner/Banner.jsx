9a3a674bc80968d6c466cdaa11e178bcddf12e57
Monjur Elahi Touheed <monjurelahitouheed@gmail.com>
2025-06-18 10:32:44 +0600

    make banner part

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,15 +1,18 @@
 import Button from "../Layout/Button";
 import Container from "../Layout/Container";
-
+import banner from '../../assets/banner.png'
 const Banner = () => {
     return (
-        <section className="pt-[41px]">
+        <section className="pt-[156px] bg-linear-to-r from-[#7E90FE]/5 to-[#9873FF]/5">
             <Container>
-                <div className="flex">
+                <div className="flex flex-col md:flex-row justify-between items-center">
                     <div>
-                        <h1 className="font-primary font-extrabold text-[80px] text-dark-primary w-[570px]">One Step Closer To Your <span className="text-transparent bg-clip-text bg-linear-to-r from-gradinet-from to-gradinet-to">Dream Job</span></h1>
+                        <h1 className="font-primary font-extrabold text-[80px] text-dark-primary w-[570px] leading-[100px]">One Step Closer To Your <span className="text-transparent bg-clip-text bg-linear-to-r from-gradinet-from to-gradinet-to">Dream Job</span></h1>
                         <p className="mt-4 font-primary font-medium text-lg text-dark-third w-[519px]">Explore thousands of job opportunities with all the information you need. Its your future. Come find it. Manage all your job application from start to finish.</p>
                         <Button className='px-7 py-[19px] rounded-lg mt-8'>Get Started</Button>
+                    </div>
+                    <div>
+                        <img src={banner} alt="" />
                     </div>
                 </div>
             </Container>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -12,37 +12,40 @@ const Header = () => {
 
       <li><NavLink to='/applied' className={({ isActive }) =>
         `font-bold font-primary text-base ${isActive ? "text-transparent bg-clip-text bg-linear-to-r from-gradinet-from to-gradinet-to bg-transparent" : "hover:text-violet-500 hover:bg-transparent"
-        }` 
+        }`
       }>Applied Jobs</NavLink></li>
     </>
 
   return (
-    <Container>
-      <div className="navbar bg-tranparent p-5 pt-[40px] md:pt-[50px] md:p-0">
-        <div className="navbar-start">
-          <div className="dropdown">
-            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+    <nav className="absolute w-full">
+      <Container>
+        <div className="navbar bg-tranparent p-5 pt-[40px] md:pt-[50px] md:p-0">
+          <div className="navbar-start">
+            <div className="dropdown">
+              <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+              </div>
+              <ul
+                tabIndex={0}
+                className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+                {links}
+              </ul>
             </div>
-            <ul
-              tabIndex={0}
-              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+            <Link to='/' className="font-primary font-extrabold text-dark-primary text-[32px]">CareerHub</Link>
+          </div>
+          <div className="navbar-center hidden lg:flex">
+            <ul className="gap-[42px] menu-horizontal px-1 font-primary font-bold text-base">
               {links}
             </ul>
           </div>
-          <Link to='/' className="font-primary font-extrabold text-dark-primary text-[32px]">CareerHub</Link>
+          <div className="navbar-end">
+            <Button className='px-7 py-[19px] rounded-lg'>Star Applying</Button>
+          </div>
         </div>
-        <div className="navbar-center hidden lg:flex">
-          <ul className="gap-[42px] menu-horizontal px-1 font-primary font-bold text-base">
-            {links}
-          </ul>
-        </div>
-        <div className="navbar-end">
-          <Button className='px-7 py-[19px] rounded-lg'>Star Applying</Button>
-        </div>
-      </div>
 
-    </Container>
+      </Container>
+    </nav>
+
   );
 };
 
